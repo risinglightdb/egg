ec16399f7e331c4fdfe128bc4cfe174d53b7702d
Alex Chi <iskyzh@gmail.com>
2023-02-19 13:00:32 -0500

    use indexmap for all maps
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/util.rs
+++ src/util.rs
@@ -41,8 +41,8 @@ pub use symbol_table::GlobalSymbol as Symbol;
 
 pub(crate) type BuildHasher = fxhash::FxBuildHasher;
 
-pub(crate) type HashMap<K, V> = hashbrown::HashMap<K, V, BuildHasher>;
-pub(crate) type HashSet<K> = hashbrown::HashSet<K, BuildHasher>;
+pub(crate) type HashMap<K, V> = indexmap::IndexMap<K, V, BuildHasher>;
+pub(crate) type HashSet<K> = indexmap::IndexSet<K, BuildHasher>;
 
 pub(crate) type IndexMap<K, V> = indexmap::IndexMap<K, V, BuildHasher>;
 pub(crate) type IndexSet<K> = indexmap::IndexSet<K, BuildHasher>;
